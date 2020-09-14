3b868b4c789f5d44371726e6d4dbcc43882de18a
Hackamo <corentin.rodrigo@2019.icam.fr>
2020-09-14 18:08:03 -0400

    update

--- movies.js
+++ movies.js
@@ -1,23 +1,28 @@
-let pagination = 1
-const french = 'fr'
-const english = 'en-us'
-let languageSelected = french
+let pagination = 1;
+const french = 'fr';
+const english = 'en-us';
+let languageSelected = french;
 
-const api_url = 'https://api.themoviedb.org/3'
+const api_url = 'https://api.themoviedb.org/3';
 let api_popularity = '/discover/movie?api_key=399af3fea42fd17a119ef910e475a6c5&sort_by=popularity.desc&include_adult=false&include_video=false&page=';
 let api_search = '/search/movie?&api_key=399af3fea42fd17a119ef910e475a6c5&query=';
-let api_vote = '/discover/movie?api_key=399af3fea42fd17a119ef910e475a6c5&sort_by=vote_count.desc&include_adult=false&include_video=false&page='
-let api_release = '/discover/movie?api_key=399af3fea42fd17a119ef910e475a6c5&sort_by=release_date.desc&include_adult=false&include_video=false&page='
-let api_revenue = '/discover/movie?api_key=399af3fea42fd17a119ef910e475a6c5&sort_by=revenue.desc&include_adult=false&include_video=false&page='
-let api_note = '/discover/movie?api_key=399af3fea42fd17a119ef910e475a6c5&sort_by=primary_release_date.desc&include_adult=false&include_video=false&page='
-let api_genre_fr = '/genre/movie/list?api_key=399af3fea42fd17a119ef910e475a6c5&language=fr'
+let api_vote = '/discover/movie?api_key=399af3fea42fd17a119ef910e475a6c5&sort_by=vote_count.desc&include_adult=false&include_video=false&page=';
+let api_release = '/discover/movie?api_key=399af3fea42fd17a119ef910e475a6c5&sort_by=release_date.desc&include_adult=false&include_video=false&page=';
+let api_revenue = '/discover/movie?api_key=399af3fea42fd17a119ef910e475a6c5&sort_by=revenue.desc&include_adult=false&include_video=false&page=';
+// let api_note = '/discover/movie?api_key=399af3fea42fd17a119ef910e475a6c5&sort_by=primary_release_date.desc&include_adult=false&include_video=false&page=';
+let api_genre_fr = '/genre/movie/list?api_key=399af3fea42fd17a119ef910e475a6c5&language=fr';
+// let api_cast_fr = '';
+let video_key = '';
+
 
 const imgPath = 'https://image.tmdb.org/t/p/w1280';
-let categorySelected = api_popularity
-let index = 0
-let listMovies = []
-let genresResp = []
-let genres = []
+let categorySelected = api_popularity;
+let index = 0;
+let listMovies = [];
+let genresResp = [];
+let genres = [];
+
+
 const main = document.getElementById('main');
 const form = document.getElementById('form');
 
@@ -25,115 +30,147 @@ const popularity = document.getElementById('popularity');
 const vote = document.getElementById('vote');
 const release = document.getElementById('release');
 const revenue = document.getElementById('revenue');
-const note = document.getElementById('note');
+// const note = document.getElementById('note');
 const fr = document.getElementById('fr');
 const eng = document.getElementById('eng');
 const search = document.getElementById('search');
-const loading = document.querySelector('.loading');
+// const loading = document.querySelector('.loading');
 
 
-// initially get fav movies
-openModal()
-getMovies(api_url + categorySelected + pagination + '&language=' + languageSelected)
-addPagination()
-getMoviesGenre(api_url + api_genre_fr)
-
+openModal();
+getMovies(api_url + categorySelected + pagination + '&language=' + languageSelected).then(r => console.log(r));
+addPagination();
+getMoviesGenre(api_url + api_genre_fr).then(r => console.log(r));
+printCast()
 
 function openModal() {
-    // Get the modal
+    let btn = document.getElementById("main");
+    let span = document.getElementsByClassName("close")[0];
     let modal = document.getElementById("myModal");
 
-    // Get the button that opens the modal
-
-    let btn = document.getElementById("main");
-
-    // Get the <span> element that closes the modal
-    let span = document.getElementsByClassName("close")[0];
-
-    // When the user clicks the button, open the modal
     btn.onclick = function () {
         modal.style.display = "block";
-    }
+    };
 
-    // When the user clicks on <span> (x), close the modal
     span.onclick = function () {
         modal.style.display = "none";
-    }
+    };
 
-    // When the user clicks anywhere outside of the modal, close it
     window.onclick = function (event) {
         if (event.target === modal) {
             modal.style.display = "none";
         }
-    }
-
+    };
 }
 
 function movieId(movies) {
     movies.forEach(movie => {
-        listMovies.push(movie)
-    })
+        listMovies.push(movie);
+    });
 
-    // let {poster_path, title, vote_average, overview, backdrop_path,original_language,release_date,genre_ids} = movies;
 
-    $('#main').on('click', 'div.overview' & 'img', function (e) {
-        const movieEl = document.getElementById(e.target.parentElement.id);
+    $('#main').on('click', 'img', function (e) {
         let modal = document.getElementById('myModal');
+        let movie = listMovies[e.target.parentElement.id];
+        let {id, title, vote_average, overview, backdrop_path, release_date, genre_ids, vote_count} = movie;
+        // getMoviesCast(api_url + '/movie/' + movie.id + '/credits?api_key=399af3fea42fd17a119ef910e475a6c5').then(r => console.log(r))
 
+        // getMoviesVideo(api_url + '/movie/' +  movie.id + '/videos?api_key=399af3fea42fd17a119ef910e475a6c5');
+
+
+        console.log(movie);
         modal.innerHTML = `
     <div class="modal-content">
-        <span class="close">&times;</span>
         
-        <img src="${imgPath + listMovies[e.target.parentElement.id].backdrop_path}" alt="${listMovies[e.target.parentElement.id].title}">
+        <img src="${imgPath + backdrop_path}" alt="${title}">
         
         <div class="movie-modal">
+             <div class="movie-modal-note">${vote_average}</div>
+             <div class="movie-modal-title">${title}</div>
+              <div class = movie-modal-release-date>Date de sortie: ${release_date}</div>
+             <div class="movie-modal-overview">${overview}</div>
+<!--             <div class="movie-modal-votes">${vote_count}</div>-->
+             <p>${showMoviesGenre(genre_ids)}</p>
+             <div>${id}</div>
+<!--             <iframe  src="https://www.youtube.com/embed/${video_key}" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>         -->
 
-             <div class="movie-modal-note">${listMovies[e.target.parentElement.id].vote_average}</div>
-             <div class="movie-modal-title">${listMovies[e.target.parentElement.id].title}</div>
-             <div class="movie-modal-overview">${listMovies[e.target.parentElement.id].overview}</div>
-             <div class = movie-modal-release-date>Date de sortie: ${listMovies[e.target.parentElement.id].release_date}</div>
-<!--             <div class="movie-modal-votes">${listMovies[e.target.parentElement.id].vote_count}</div>-->
-             <p>Genre: ${listMovies[e.target.parentElement.id].genre_ids}</p>
-<!--             <p>${genres[0]}</p>-->
          </div>
-         
     </div>
         `;
-        main.appendChild(modal)
+        main.appendChild(modal);
     });
 }
 
 
+
 function addPagination() {
-    pagination += 1
+    pagination += 1;
 }
 
 
 async function getMovies(url) {
     const resp = await fetch(url);
     const respData = await resp.json();
-    showMovies(respData.results)
-    movieId(respData.results)
+    showMovies(respData.results);
+     movieId(respData.results);
 }
 
 async function getMoviesGenre(url) {
     const resp = await fetch(url);
     const respData = await resp.json();
-    let keys = Object.keys(respData)
-    keys.forEach(function (key){
-        genresResp.push(respData[key])
+    let keys = Object.keys(respData);
+    keys.forEach(function (key) {
+        genresResp.push(respData[key]);
     })
-    let genresTrue = genresResp[0]
+    let genresTrue = genresResp[0];
     genresTrue.forEach(genre => {
-        genres.push({key:genre.id, value:genre.name})
+        genres.push({key: genre.id, value: genre.name});
     })
+}
+
+async function getMoviesVideo(url) {
+    const resp = await fetch(url);
+    const respData = await resp.json();
+    showMoviesVideo(respData.results);
+}
+
+
+async function getMoviesCast(url) {
+    const resp = await fetch(url);
+    const respData = await resp.json();
+    showMovieCast(respData.cast);
+}
+
+function showMovieCast(caster) {
+    let casting = [];
+    caster.forEach(cast => {
+        casting.push(" " + cast.name);
+    });
+    let casting_String = ''
+    casting.forEach(cast => {
+        casting_String.concat()
+    })
+    return (casting_String)
+}
+
+function showMoviesVideo(videos) {
+    video_key = videos[0].key;
+}
+
+function showMoviesGenre(genreTargeted) {
+    let listGenres = [];
+    genres.forEach(genre => {
+        genreTargeted.forEach(genreSelected => {
+            if (genre.key === genreSelected) {
+                listGenres.push(" " + genre.value);
+            }
+        })
+    })
+    return listGenres
 }
 
 
 function showMovies(movies) {
-    //clear main
-    // main.innerHTML = "";
-
     movies.forEach(movie => {
 
         const {poster_path, title, vote_average, overview, backdrop_path} = movie;
@@ -141,11 +178,7 @@ function showMovies(movies) {
         if (poster_path == null) {
         } else {
             movieEl.classList.add('movie');
-            movieEl.setAttribute('id', index)
-            let getTargetId = document.getElementById(index)
-            // movieEl.classList.add('portfolio-item');
-            // movieEl.setAttribute('data-toggle','modal')
-            // movieEl.setAttribute('data-target',"#preview")
+            movieEl.setAttribute('id', index);
             movieEl.innerHTML = `
         <img src="${imgPath + poster_path}" alt="${title}">
         <div class="movie-info">
@@ -157,12 +190,13 @@ function showMovies(movies) {
         ${overview}
         </div>
         `;
-            main.appendChild(movieEl)
+            main.appendChild(movieEl);
             // loading.classList.remove('show');
         }
-        index += 1
+        index += 1;
     });
-    console.log(movies)
+    console.log(movies);
+    console.log(typeof movies);
 }
 
 
@@ -183,7 +217,7 @@ form.addEventListener('submit', (e) => {
     categorySelected = null
     if (searchTerm) {
         main.innerHTML = "";
-        getMovies(api_url + api_search + searchTerm)
+        getMovies(api_url + api_search + searchTerm).then(r => console.log(r))
         search.value = '';
     }
 });
@@ -193,7 +227,7 @@ popularity.addEventListener('click', (e) => {
     e.preventDefault();
     main.innerHTML = "";
     categorySelected = api_popularity;
-    getMovies(api_url + categorySelected + pagination + '&language=' + languageSelected);
+    getMovies(api_url + categorySelected + pagination + '&language=' + languageSelected).then(r => console.log(r));
     addPagination();
 });
 
@@ -203,7 +237,7 @@ vote.addEventListener('click', (e) => {
     e.preventDefault();
     main.innerHTML = "";
     categorySelected = api_vote;
-    getMovies(api_url + categorySelected + pagination + '&language=' + languageSelected);
+    getMovies(api_url + categorySelected + pagination + '&language=' + languageSelected).then(r => console.log(r));
     addPagination();
 });
 
@@ -212,9 +246,9 @@ release.addEventListener('click', (e) => {
     e.preventDefault();
     main.innerHTML = "";
     categorySelected = api_release;
-    getMovies(api_url + categorySelected + pagination + '&language=' + languageSelected);
+    getMovies(api_url + categorySelected + pagination + '&language=' + languageSelected).then(r => console.log(r));
     addPagination();
-    getMovies(api_url + categorySelected + pagination + '&language=' + languageSelected);
+    getMovies(api_url + categorySelected + pagination + '&language=' + languageSelected).then(r => console.log(r));
     addPagination();
 });
 
@@ -223,7 +257,7 @@ revenue.addEventListener('click', (e) => {
     e.preventDefault();
     main.innerHTML = "";
     categorySelected = api_revenue;
-    getMovies(api_url + categorySelected + pagination + '&language=' + languageSelected);
+    getMovies(api_url + categorySelected + pagination + '&language=' + languageSelected).then(r => console.log(r));
     addPagination();
 
 });
@@ -233,7 +267,7 @@ fr.addEventListener('click', (e) => {
     e.preventDefault();
     main.innerHTML = "";
     languageSelected = french;
-    getMovies(api_url + categorySelected + pagination + '&language=' + languageSelected);
+    getMovies(api_url + categorySelected + pagination + '&language=' + languageSelected).then(r => console.log(r));
 
 });
 
@@ -242,7 +276,7 @@ eng.addEventListener('click', (e) => {
     e.preventDefault();
     main.innerHTML = "";
     languageSelected = english;
-    getMovies(api_url + categorySelected + pagination + '&language=' + languageSelected);
+    getMovies(api_url + categorySelected + pagination + '&language=' + languageSelected).then(r => console.log(r));
 
 });
 
@@ -264,7 +298,7 @@ function showLoading() {
     // loading.classList.add('show');
 
     // load more data
-    getMovies(api_url + categorySelected + pagination + '&language=' + languageSelected)
+    getMovies(api_url + categorySelected + pagination + '&language=' + languageSelected).then(r => console.log(r))
     addPagination()
 }
 
